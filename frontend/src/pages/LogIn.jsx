f23481db602d15b5e7872ca6fd7689b37aca1142
Suchismita04 <suchismitachatterjee13@gmail.com>
2024-03-10 11:48:26 +0530

    connect frontend with backend

--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 const LogIn = () => {
+    const [logInFormData,setlogInFormData]=useState([
+        email='',
+        passoward=''
+    ])
+    const handleChange=(event)=>{
+       return setlogInFormData({...logInFormData,[event.target.name]:event.target.value})
+    }
+    const handleSubmit=(event)=>{
+        event.preventDefult()
+        try {
+            const responce=axios.post('/api/v1/users/logIn',logInFormData)
+            if (responce.status === 200) {
+                setlogInFormData(responce.data.message)
+            }
+            else{
+                setlogInFormData(responce.data.message)
+            }
+        } catch (error) {
+            console.log("Error during logIn",error)
+        }
+    }
+    const handleButtonClick=()=>{
+        handleSubmit()
+    }
     return (
         <>
             <div >
-                <form action="/api/v1/users" method="post" className='container border border-success-subtle rounded mx-2 my-3'>
+                <form action="/api/v1/users/logIn" method="post" className='container border border-success-subtle rounded mx-2 my-3'>
                     <button type="button" className="btn-close d-flex my-3 " aria-label="Close" style={{ "position": "relative", "left": "15rem" }}></button>
                     <img src="/public/login-icon.jpg" alt="login-icon" className='d-flex img-fluid' style={{"height": "13rem",
     "width": "16rem"}}/>
                     <div className="input-group flex-nowrap my-3 ">
                         <span className="input-group-text" id="addon-wrapping">@</span>
-                        <input type="text" className="form-control" placeholder="Email" aria-label="Email" aria-describedby="addon-wrapping" />
+                        <input  type="email" onChange={handleChange} className="form-control" name='email' value={logInFormData.email} placeholder="Email" aria-label="Email" aria-describedby="addon-wrapping" />
                     </div>
                     <div className="input-group flex-nowrap my-3 ">
                         <span className="input-group-text" id="addon-wrapping">@</span>
-                        <input type="password" className="form-control " placeholder="password" aria-label="password" aria-describedby="addon-wrapping" />
+                        <input type="password" onChange={handleChange} className="form-control " name='password' value={logInFormData.passoward} placeholder="password" aria-label="password" aria-describedby="addon-wrapping" />
                     </div>
-                    <input className="btn btn-primary my-3 rounded" type="submit" value="Sign In"></input>
+                    <button onClick={handleButtonClick} className="btn btn-primary my-3 rounded" type="submit" value="LogIn"></button>
                     <Link to="/form/forgetPassword" className='text-decoration-underline d-flex' style={{
                         "position": "relative",
                         "left": "68px"
