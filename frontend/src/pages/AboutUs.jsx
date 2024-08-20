7ad59f7bd6b4217a8e8d0de98e705d1ecab10cd4
Suchismita04 <suchismitachatterjee13@gmail.com>
2024-08-20 22:22:01 +0530

    debug

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -4,15 +4,19 @@ import '../styles/AboutUs.css'
 function AboutUs() {
     return (
         <>
-            <div className="container1 ">
-                <div className="content">
+
+        
+            <div className="container1  ">
+                <div className="content col-lg-6 col-md-12 text-center text-lg-left">
                     <h3 className='my-3'>  Welcome to CreezyShiksha, where learning meets innovation! </h3>
                     <div className='my-3 des'>
                         Our mission is to revolutionize education through technology, offering accessible, engaging, and personalized learning experiences for all. With a dedicated team of educators, developers, and designers, we empower learners to reach their full potential. Whether you're a student, teacher, or lifelong learner, Queviz is your gateway to knowledge and inspiration.
                     </div>
                 </div>
 
-                <div className='img1'> <img src="/aboutUsImg.png" alt="error" /></div>
+                <div className='img1'> 
+                    <img src="/aboutUsImg.png" alt="error" />
+                    </div>
             </div>
 
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavLink } from 'react-router-dom';
 import "../styles/Home.css"
 import "../styles/style.css"
+// import "../styles/Responsive.css"
 
 
 const Home = () => {
@@ -11,15 +12,15 @@ const Home = () => {
    <div className='home-wrapper'>
 
 
-      <div className='container-fluid vw-100 vh-100  d-flex  align-items-center justify-content-center m-0 p-0' >
+      <div className='container-fluid vw-100 vh-100   align-items-center justify-content-center m-0 p-0' >
         <div className='container w-100 d-flex align-items-center'>
           <div className="row w-100 h-100  align-items-center  m-0">
-            <div className='col-lg text-center text-lg-left'>
+            <div className='col-lg-6 col-md-12 text-center text-lg-left'>
               <h1 className='d-flex my-3 f-color '>Hey there,</h1>
-              <h2 className='d-flex my-2 animated-text f-color '>Welcome to CreezyShiksha</h2>
+              <h2 className='d-flex my-2 animated-text f-color  '>Welcome to CreezyShiksha</h2>
               <NavLink to="/form/signIn" role='button ' className="btn my-4 btn-primary" >Sign In</NavLink>
             </div>
-            <div className='col-lg image text-center text-lg-right'>
+            <div className='col-lg-6 col-md-12 text-center text-lg-right image'>
               <img src="/HomeImg.png" alt="error" className='img-fluid'/>
              
             </div>
